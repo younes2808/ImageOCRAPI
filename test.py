a9c821e5ca130c32a54f3f2df331865ca2325140
younes2808 <benhaida.mehdi@gmail.com>
2025-02-10 12:40:37 +0100

    Legger inn /upload dynamically

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 import requests
+from urllib.parse import urljoin
 
-# API endpoint (replace with your actual LocalTunnel URL)
-url = "" # Change when localtunnel is on
+# Base API URL (Change this as needed)
+base_url = "https://slick-papers-beam.loca.lt/"
 
-# Path to the image file you want to send
-image_path = "./036.png"
+# "/upload" is appended after the url
+upload_url = urljoin(base_url.rstrip("/") + "/", "upload")
+
+# Path to the image file
+image_path = "./058.png"
 
 # Send the image using POST request
 with open(image_path, "rb") as file:
     files = {"file": file}
-    response = requests.post(url, files=files)
+    response = requests.post(upload_url, files=files)
 
 # Print the response (should return LaTeX code)
 print(response.json())
